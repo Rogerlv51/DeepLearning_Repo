30eee000fb696d129437dc5dad9574d198ab5a03
Rogerlv51 <949608370@qq.com>
2022-07-06 16:37:56 +0800

    完成Transformer

--- Transformer.py
+++ Transformer.py
@@ -170,5 +170,62 @@ state = [encoder_blk(X, valid_lens), valid_lens, [None]]
 print(decoder_blk(X, state)[0].shape)
 
 
+# 构建transformer解码器，还有后面全连接层输出
+class TransformerDecoder(nn.Module):
+    def __init__(self, vocab_size, key_size, query_size, value_size,
+                num_hiddens, norm_shape, ffn_num_input, ffn_num_hiddens,
+                num_heads, num_layers, dropout, **kwargs):
+        super(TransformerDecoder, self).__init__(**kwargs)
+        self.num_hiddens = num_hiddens
+        self.num_layers = num_layers
+        self.embedding = nn.Embedding(vocab_size, num_hiddens)
+        self.pos_encoding = PositionalEncoding(num_hiddens, dropout)
+        self.blks = nn.Sequential()
+        for i in range(num_layers):
+            self.blks.add_module("block"+str(i),
+                    DecoderBlock(key_size, query_size, value_size, num_hiddens, 
+                    norm_shape, ffn_num_input, ffn_num_hiddens, num_heads, dropout, i))
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+
+    def init_state(self, enc_outputs, enc_valid_lens, *args):
+        return [enc_outputs, enc_valid_lens, [None] * self.num_layers]
+
+    def forward(self, X, state):
+        X = self.pos_encoding(self.embedding(X) * math.sqrt(self.num_hiddens))
+        self._attention_weights = [[None] * len(self.blks) for _ in range (2)]
+        for i, blk in enumerate(self.blks):
+            X, state = blk(X, state)
+            # 解码器⾃注意⼒权重
+            self._attention_weights[0][i] = blk.attention1.attention.attention_weights
+            # “编码器－解码器”⾃注意⼒权重
+            self._attention_weights[1][i] = blk.attention2.attention.attention_weights
+        return self.dense(X), state
+
+    @property
+    def attention_weights(self):
+        return self._attention_weights
+
+
+if __name__ == '__main__':
+    num_hiddens, num_layers, dropout, batch_size, num_steps = 32, 2, 0.1, 64, 10
+    lr, num_epochs, device = 0.005, 200, d2l.try_gpu()
+    ffn_num_input, ffn_num_hiddens, num_heads = 32, 64, 4
+    key_size, query_size, value_size = 32, 32, 32
+    norm_shape = [32]
+    train_iter, src_vocab, tgt_vocab = d2l.load_data_nmt(batch_size, num_steps)
+    encoder = TransformerEncoder(
+        len(src_vocab), key_size, query_size, value_size, num_hiddens,
+        norm_shape, ffn_num_input, ffn_num_hiddens, num_heads,
+        num_layers, dropout)
+    decoder = TransformerDecoder(
+        len(tgt_vocab), key_size, query_size, value_size, num_hiddens,
+        norm_shape, ffn_num_input, ffn_num_hiddens, num_heads,
+        num_layers, dropout)
+    net = d2l.EncoderDecoder(encoder, decoder)
+    d2l.train_seq2seq(net, train_iter, lr, num_epochs, tgt_vocab, device)
+
+
+
+
 
 
